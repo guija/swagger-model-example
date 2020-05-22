dfcd8a76831d51514068aed4ef85c5f4f13ace60
Guillermo Janner Acero <Guillermo.Janner-Acero@bmw.de>
2020-05-22 20:18:47 +0200

    Initial commit

---
+++ src/main/java/io/github/guija/swaggermodelexample/SwaggerModelExampleApplication.java
@@ -0,0 +1,13 @@
+package io.github.guija.swaggermodelexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerModelExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerModelExampleApplication.class, args);
+	}
+
+}
